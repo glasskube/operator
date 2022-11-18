1d7863022a9e50c55df2d8af661cbf23cbfcdb57
Jakob Steiner <jakob.steiner@glasskube.eu>
2022-11-18 14:34:28 +0100

    feat: add generating matomo db password (#15)

--- operator/src/main/kotlin/eu/glasskube/operator/matomo/dependent/MatomoSecret.kt
+++ operator/src/main/kotlin/eu/glasskube/operator/matomo/dependent/MatomoSecret.kt
@@ -6,21 +6,25 @@ import eu.glasskube.operator.matomo.Matomo
 import eu.glasskube.operator.matomo.MatomoReconciler
 import eu.glasskube.operator.matomo.resourceLabels
 import eu.glasskube.operator.matomo.secretName
+import eu.glasskube.operator.secrets.SecretGenerator
 import io.fabric8.kubernetes.api.model.Secret
 import io.javaoperatorsdk.operator.api.reconciler.Context
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernetesDependentResource
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependent
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.ResourceUpdatePreProcessor
 
 @KubernetesDependent(labelSelector = MatomoReconciler.SELECTOR)
-class MatomoSecret : CRUDKubernetesDependentResource<Secret, Matomo>(Secret::class.java) {
+class MatomoSecret :
+    CRUDKubernetesDependentResource<Secret, Matomo>(Secret::class.java),
+    ResourceUpdatePreProcessor<Secret> {
     override fun desired(primary: Matomo, context: Context<Matomo>) = secret {
         metadata {
             name = primary.secretName
             namespace = primary.metadata.namespace
-            labels = primary.resourceLabels
+            labels = primary.resourceLabels + SecretGenerator.LABEL
+            annotations = mapOf(SecretGenerator.generateKeys("MATOMO_DATABASE_PASSWORD"))
         }
-        stringData = mapOf(
-            "MATOMO_DATABASE_PASSWORD" to ""
-        )
     }
+
+    override fun replaceSpecOnActual(actual: Secret, desired: Secret, context: Context<*>) = actual
 }
--- operator/src/main/kotlin/eu/glasskube/operator/secrets/SecretGenerator.kt
+++ operator/src/main/kotlin/eu/glasskube/operator/secrets/SecretGenerator.kt
@@ -5,23 +5,24 @@ import io.javaoperatorsdk.operator.api.reconciler.Context
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl
+import org.apache.commons.lang3.RandomStringUtils
 import org.slf4j.LoggerFactory
-import java.util.*
+import java.util.Random
 
-private val log = LoggerFactory.getLogger(SecretGeneratorReconciler::class.java)
+private val log = LoggerFactory.getLogger(SecretGenerator::class.java)
 
 @ControllerConfiguration(
-    labelSelector = SecretGeneratorReconciler.LABEL,
+    labelSelector = SecretGenerator.LABEL_SELECTOR,
     generationAwareEventProcessing = false
 )
-class SecretGeneratorReconciler(private val random: Random) : Reconciler<Secret> {
+class SecretGenerator(private val random: Random) : Reconciler<Secret> {
     override fun reconcile(resource: Secret, context: Context<Secret>): UpdateControl<Secret> {
         log.info("Reconciling ${resource.kind} ${resource.metadata.name}@${resource.metadata.namespace}")
         val generateKeys = resource.metadata.annotations[GENERATE_KEYS]?.split(',')?.toSet().orEmpty()
         val generatedKeys = resource.metadata.annotations[GENERATED_KEYS]?.split(',')?.toSet().orEmpty()
 
-        log.debug("Desired keys: $generateKeys")
-        log.debug("Existing keys: $generatedKeys")
+        log.info("Desired keys: $generateKeys")
+        log.info("Existing keys: $generatedKeys")
 
         if (generateKeys == generatedKeys) {
             log.info("No update required")
@@ -30,17 +31,22 @@ class SecretGeneratorReconciler(private val random: Random) : Reconciler<Secret>
 
         log.info("Update required")
 
-        (generateKeys - generatedKeys).forEach { resource.stringData[it] = random.nextInt().toString() }
+        (generateKeys - generatedKeys).forEach { resource.stringData[it] = random.nextString(32) }
         (generatedKeys - generateKeys).forEach { resource.data.remove(it) }
 
-        resource.metadata.annotations[GENERATED_KEYS] = generateKeys.joinToString(separator = ",")
+        resource.metadata.annotations[GENERATED_KEYS] = generateKeys.joinToString(",")
 
         return UpdateControl.updateResource(resource)
     }
 
+    private fun Random.nextString(count: Int) =
+        RandomStringUtils.random(count, 0, 0, true, true, null, this)
+
     companion object {
-        const val LABEL = "glasskube.eu/generated=yes"
+        val LABEL = "glasskube.eu/generated" to "yes"
+        const val LABEL_SELECTOR = "glasskube.eu/generated=yes"
         const val GENERATE_KEYS = "secrets.glasskube.eu/generateKeys"
         const val GENERATED_KEYS = "secrets.glasskube.eu/generatedKeys"
+        fun generateKeys(vararg keys: String) = GENERATE_KEYS to keys.joinToString(",")
     }
 }
