dc134037953111cd45f3ab82e3e9a4f105bcb5ad
Philip Miglinci <pmig@glasskube.eu>
2022-12-21 12:46:19 +0100

    feat: make upgrade script executable and add multi namespace hint
    Signed-off-by: Philip Miglinci <pmig@glasskube.eu>

--- deploy/upgrade.sh
+++ deploy/upgrade.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Currently only multi namespace deployments are supported
+
 helm upgrade cert-manager jetstack/cert-manager -n cert-manager
 helm upgrade kube-prometheus-stack prometheus-community/kube-prometheus-stack -n kube-prometheus-stack
 helm upgrade mariadb-operator mmontes/mariadb-operator -n mariadb-system --set ha.enabled=false
