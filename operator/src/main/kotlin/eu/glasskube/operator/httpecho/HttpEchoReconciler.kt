eacc22c8c17b4826d4b6a166437d3ba15b060c8f
Jakob Steiner <jakob.steiner@glasskube.eu>
2023-01-11 14:07:29 +0100

    refactor(HttpEcho): change postgres secrets to use discriminators (#39)

--- operator/src/main/kotlin/eu/glasskube/operator/httpecho/HttpEchoReconciler.kt
+++ operator/src/main/kotlin/eu/glasskube/operator/httpecho/HttpEchoReconciler.kt
@@ -1,8 +1,17 @@
 package eu.glasskube.operator.httpecho
 
-import eu.glasskube.operator.httpecho.dependent.*
+import eu.glasskube.operator.api.reconciler.informerEventSource
+import eu.glasskube.operator.httpecho.dependent.HttpEchoDeployment
+import eu.glasskube.operator.httpecho.dependent.HttpEchoIngress
+import eu.glasskube.operator.httpecho.dependent.HttpEchoPostgres
+import eu.glasskube.operator.httpecho.dependent.HttpEchoPostgresAppSecret
+import eu.glasskube.operator.httpecho.dependent.HttpEchoPostgresSuperuserSecret
+import eu.glasskube.operator.httpecho.dependent.HttpEchoService
+import io.fabric8.kubernetes.api.model.Secret
 import io.javaoperatorsdk.operator.api.reconciler.Context
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration
+import io.javaoperatorsdk.operator.api.reconciler.EventSourceContext
+import io.javaoperatorsdk.operator.api.reconciler.EventSourceInitializer
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl
 import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent
@@ -16,20 +25,33 @@ private val LOG = LoggerFactory.getLogger(HttpEchoReconciler::class.java)
         Dependent(type = HttpEchoService::class),
         Dependent(type = HttpEchoIngress::class),
         Dependent(type = HttpEchoPostgres::class),
-        Dependent(type = HttpEchoPostgresSuperuserSecret::class),
-        Dependent(type = HttpEchoPostgresAppSecret::class)
+        Dependent(
+            type = HttpEchoPostgresSuperuserSecret::class,
+            useEventSourceWithName = HttpEchoReconciler.SECRETS_EVENT_SOURCE_NAME
+        ),
+        Dependent(
+            type = HttpEchoPostgresAppSecret::class,
+            useEventSourceWithName = HttpEchoReconciler.SECRETS_EVENT_SOURCE_NAME
+        )
     ]
 )
-class HttpEchoReconciler : Reconciler<HttpEcho> {
+class HttpEchoReconciler : Reconciler<HttpEcho>, EventSourceInitializer<HttpEcho> {
     override fun reconcile(resource: HttpEcho, context: Context<HttpEcho>): UpdateControl<HttpEcho> {
         LOG.info("reconciling ${resource.crdName} ${resource.apiVersion}")
         resource.status = HttpEchoStatus("Echoing")
         return UpdateControl.patchStatus(resource)
     }
 
+    override fun prepareEventSources(context: EventSourceContext<HttpEcho>) = with(context) {
+        mutableMapOf(
+            SECRETS_EVENT_SOURCE_NAME to informerEventSource<Secret>()
+        )
+    }
+
     companion object {
         const val LABEL = "glasskube.eu/HttpEcho"
         const val APP_NAME = "http-echo"
         const val SELECTOR = "app.kubernetes.io/managed-by=glasskube-operator,app=$APP_NAME"
+        const val SECRETS_EVENT_SOURCE_NAME = "HttpEchoSecretEventSource"
     }
 }
