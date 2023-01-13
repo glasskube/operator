f1179c85ac96196c9e876be96f8627f976dcda00
Jakob Steiner <jakob.steiner@glasskube.eu>
2023-01-13 15:37:47 +0100

    refactor: add DependentIngress to encapsulate cloud vendor defaults for ingress resources (#52)

--- operator/src/main/kotlin/eu/glasskube/operator/matomo/dependent/MatomoIngress.kt
+++ operator/src/main/kotlin/eu/glasskube/operator/matomo/dependent/MatomoIngress.kt
@@ -6,72 +6,30 @@ import eu.glasskube.kubernetes.api.model.extensions.ingressPath
 import eu.glasskube.kubernetes.api.model.extensions.ingressRuleValue
 import eu.glasskube.kubernetes.api.model.extensions.spec
 import eu.glasskube.kubernetes.api.model.metadata
-import eu.glasskube.operator.config.CloudProvider
-import eu.glasskube.operator.getCloudProvider
+import eu.glasskube.operator.generic.dependant.DependentIngress
 import eu.glasskube.operator.matomo.Matomo
 import eu.glasskube.operator.matomo.MatomoReconciler
 import eu.glasskube.operator.matomo.ingressName
 import eu.glasskube.operator.matomo.ingressTlsCertName
 import eu.glasskube.operator.matomo.resourceLabels
 import eu.glasskube.operator.matomo.serviceName
-import io.fabric8.kubernetes.api.model.networking.v1.Ingress
 import io.fabric8.kubernetes.api.model.networking.v1.IngressRule
 import io.fabric8.kubernetes.api.model.networking.v1.IngressTLS
-import io.fabric8.kubernetes.client.dsl.base.ResourceDefinitionContext
 import io.javaoperatorsdk.operator.api.reconciler.Context
-import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernetesDependentResource
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependent
 
 @KubernetesDependent(labelSelector = MatomoReconciler.SELECTOR)
-class MatomoIngress : CRUDKubernetesDependentResource<Ingress, Matomo>(Ingress::class.java) {
-    private fun configureIngressClassName(): String {
-        return when (getCloudProvider(client)) {
-            CloudProvider.aws -> "alb"
-            else -> "nginx"
-        }
-    }
-
-    private fun configureAnnotations(): Map<String, String> {
-        return when (getCloudProvider(client)) {
-            CloudProvider.aws -> mapOf(
-                "alb.ingress.kubernetes.io/listen-ports" to "[{\"HTTP\": 80}, {\"HTTPS\": 443}]",
-                "alb.ingress.kubernetes.io/scheme" to "internet-facing",
-                "alb.ingress.kubernetes.io/target-type" to "ip",
-                "alb.ingress.kubernetes.io/ssl-redirect" to "443",
-                "alb.ingress.kubernetes.io/group.name" to "glasskube"
-            )
-
-            else -> setClusterIssuerIfPresent()
-        }
-    }
-
-    private fun setClusterIssuerIfPresent(): Map<String, String> {
-        val clusterIssuerContext = ResourceDefinitionContext.Builder()
-            .withKind("ClusterIssuer")
-            .withGroup("cert-manager.io")
-            .withVersion("v1")
-            .build()
-
-        val items = client.genericKubernetesResources(clusterIssuerContext).list().items
-
-        if (items.isNotEmpty()) {
-            return mapOf(
-                "cert-manager.io/cluster-issuer" to items[0].metadata.name
-            )
-        }
-
-        return emptyMap()
-    }
+class MatomoIngress : DependentIngress<Matomo>() {
 
     override fun desired(primary: Matomo, context: Context<Matomo>) = ingress {
         metadata {
             name = primary.ingressName
             namespace = primary.metadata.namespace
             labels = primary.resourceLabels
-            annotations = configureAnnotations()
+            annotations = defaultAnnotations
         }
         spec {
-            ingressClassName = configureIngressClassName()
+            ingressClassName = defaultIngressClassName
             rules = listOf(
                 IngressRule(
                     primary.spec.host,
