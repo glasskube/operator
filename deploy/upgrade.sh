16eff853418d1ceb1ca292dc7487bf01d386bab1
Jakob Steiner <jakob.steiner@glasskube.eu>
2023-01-23 18:08:44 +0100

    refactor: change deploy.sh to provide functionality of upgrade.sh and delete upgrade.sh

--- deploy/upgrade.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Currently only multi namespace deployments are supported
-
-helm upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace
-helm upgrade --install kube-prometheus-stack prometheus-community/kube-prometheus-stack --namespace kube-prometheus-stack --create-namespace
-helm upgrade --install mariadb-operator mmontes/mariadb-operator --namespace mariadb-system --create-namespace --set ha.enabled=false
-helm upgrade --install cnpg cnpg/cloudnative-pg --namespace cnpg-system --create-namespace
