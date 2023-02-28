2607b313efbddadec2e12fc662b44e88ec43d316
Jakob Steiner <jakob.steiner@glasskube.eu>
2023-02-28 15:27:43 +0100

    feat: add utility for usage of recommended labels
    also: deprecate resourceLabels

---
+++ operator/src/main/kotlin/eu/glasskube/operator/Labels.kt
@@ -0,0 +1,38 @@
+package eu.glasskube.operator
+
+object Labels {
+    const val NAME = "app.kubernetes.io/name"
+    const val INSTANCE = "app.kubernetes.io/instance"
+    const val VERSION = "app.kubernetes.io/version"
+    const val MANAGED_BY = "app.kubernetes.io/managed-by"
+    const val COMPONENT = "app.kubernetes.io/component"
+    const val PART_OF = "app.kubernetes.io/part-of"
+    const val MANAGED_BY_GLASSKUBE = "$MANAGED_BY=glasskube-operator"
+
+    fun resourceLabels(
+        name: String,
+        instance: String,
+        partOf: String? = null,
+        version: String? = null,
+        component: String? = null
+    ) = mutableMapOf(
+        MANAGED_BY to "glasskube-operator",
+        NAME to name,
+        INSTANCE to instance,
+    ).also { labels ->
+        if (version != null) labels += VERSION to version
+        if (component != null) labels += COMPONENT to component
+        if (partOf != null) labels += PART_OF to partOf
+    }.toMap()
+
+    fun resourceLabelSelector(
+        name: String,
+        instance: String,
+        partOf: String? = null
+    ) = mutableMapOf(
+        NAME to name,
+        INSTANCE to instance,
+    ).also {
+        if (partOf != null) it += PART_OF to partOf
+    }.toMap()
+}
--- operator/src/main/kotlin/eu/glasskube/operator/Utils.kt
+++ operator/src/main/kotlin/eu/glasskube/operator/Utils.kt
@@ -2,6 +2,13 @@ package eu.glasskube.operator
 
 import java.util.Base64
 
+@Deprecated(
+    "Use Labels.resourceLabels instead to opt in to Kubernetes Recommended Labels.",
+    ReplaceWith(
+        "Labels.resourceLabels(app, TODO(\"specify instance name\"), app) + \n additionalLabels",
+        "eu.glasskube.Labels"
+    )
+)
 fun resourceLabels(app: String, vararg additionalLabels: Pair<String, String>) =
     mapOf(
         "app.kubernetes.io/managed-by" to "glasskube-operator",
