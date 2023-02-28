bc4abf958569bf551896e1c55a1a873371f0ba30
Jakob Steiner <jakob.steiner@glasskube.eu>
2023-02-28 15:05:04 +0100

    feat(Gitea): add ServiceMonitor (#81)

---
+++ operator/src/main/kotlin/eu/glasskube/operator/gitea/dependent/GiteaServiceMonitor.kt
@@ -0,0 +1,42 @@
+package eu.glasskube.operator.gitea.dependent
+
+import eu.glasskube.kubernetes.api.model.labelSelector
+import eu.glasskube.kubernetes.api.model.metadata
+import eu.glasskube.operator.gitea.Gitea
+import eu.glasskube.operator.gitea.GiteaReconciler
+import eu.glasskube.operator.gitea.genericResourceName
+import eu.glasskube.operator.gitea.resourceLabelSelector
+import eu.glasskube.operator.gitea.resourceLabels
+import eu.glasskube.operator.gitea.secretName
+import eu.glasskube.operator.prometheus.servicemonitor.EndpointSpec
+import eu.glasskube.operator.prometheus.servicemonitor.ServiceMonitor
+import eu.glasskube.operator.prometheus.servicemonitor.ServiceMonitorSpec
+import eu.glasskube.operator.prometheus.servicemonitor.serviceMonitor
+import io.fabric8.kubernetes.api.model.SecretKeySelector
+import io.javaoperatorsdk.operator.api.reconciler.Context
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernetesDependentResource
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependent
+
+@KubernetesDependent(labelSelector = GiteaReconciler.SELECTOR)
+class GiteaServiceMonitor : CRUDKubernetesDependentResource<ServiceMonitor, Gitea>(ServiceMonitor::class.java) {
+    override fun desired(primary: Gitea, context: Context<Gitea>) = serviceMonitor {
+        metadata {
+            name = primary.genericResourceName
+            namespace = primary.metadata.namespace
+            labels = primary.resourceLabels
+        }
+        spec = ServiceMonitorSpec(
+            endpoints = listOf(
+                EndpointSpec(
+                    port = "http",
+                    path = "/metrics",
+                    interval = "10s",
+                    bearerTokenSecret = SecretKeySelector("GITEA__metrics__TOKEN", primary.secretName, false)
+                ),
+            ),
+            selector = labelSelector {
+                matchLabels = primary.resourceLabelSelector
+            }
+        )
+    }
+}
