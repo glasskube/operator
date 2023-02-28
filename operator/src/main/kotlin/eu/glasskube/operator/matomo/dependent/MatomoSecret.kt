d5d9b03540db78d8ac4aefc7179c82c0f53029d6
Jakob Steiner <jakob.steiner@glasskube.eu>
2023-02-28 15:10:32 +0100

    refactor: add GeneratedSecret base class for dependent resources

--- operator/src/main/kotlin/eu/glasskube/operator/matomo/dependent/MatomoSecret.kt
+++ operator/src/main/kotlin/eu/glasskube/operator/matomo/dependent/MatomoSecret.kt
@@ -1,35 +1,15 @@
 package eu.glasskube.operator.matomo.dependent
 
-import eu.glasskube.kubernetes.api.model.metadata
-import eu.glasskube.kubernetes.api.model.secret
+import eu.glasskube.operator.generic.dependent.GeneratedSecret
 import eu.glasskube.operator.matomo.Matomo
 import eu.glasskube.operator.matomo.MatomoReconciler
 import eu.glasskube.operator.matomo.resourceLabels
 import eu.glasskube.operator.matomo.secretName
-import eu.glasskube.operator.secrets.SecretGenerator
-import io.fabric8.kubernetes.api.model.Secret
-import io.javaoperatorsdk.operator.api.reconciler.Context
-import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernetesDependentResource
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependent
-import io.javaoperatorsdk.operator.processing.dependent.kubernetes.ResourceUpdatePreProcessor
 
 @KubernetesDependent(labelSelector = MatomoReconciler.SELECTOR)
-class MatomoSecret :
-    CRUDKubernetesDependentResource<Secret, Matomo>(Secret::class.java),
-    ResourceUpdatePreProcessor<Secret> {
-    override fun desired(primary: Matomo, context: Context<Matomo>) = secret {
-        metadata {
-            name = primary.secretName
-            namespace = primary.metadata.namespace
-            labels = primary.resourceLabels + SecretGenerator.LABEL
-            annotations = mapOf(SecretGenerator.generateKeys("MATOMO_DATABASE_PASSWORD", "ROOT_DATABASE_PASSWORD"))
-        }
-    }
-
-    override fun replaceSpecOnActual(actual: Secret, desired: Secret, context: Context<*>) = actual.apply {
-        metadata.annotations.putAll(desired.metadata.annotations)
-        metadata.labels.putAll(desired.metadata.labels)
-        data.putAll(desired.data)
-        stringData.putAll(desired.stringData)
-    }
+class MatomoSecret : GeneratedSecret<Matomo>() {
+    override val Matomo.generatedSecretName get() = secretName
+    override val Matomo.generatedSecretLabels get() = resourceLabels
+    override val generatedKeys get() = arrayOf("MATOMO_DATABASE_PASSWORD", "ROOT_DATABASE_PASSWORD")
 }
--- operator/src/main/kotlin/eu/glasskube/operator/odoo/dependent/OdooDatabaseBackupSecret.kt
+++ operator/src/main/kotlin/eu/glasskube/operator/odoo/dependent/OdooDatabaseBackupSecret.kt
@@ -1,47 +1,20 @@
 package eu.glasskube.operator.odoo.dependent
 
-import eu.glasskube.kubernetes.api.model.metadata
-import eu.glasskube.kubernetes.api.model.secret
-import eu.glasskube.operator.api.reconciler.NamedResourceDiscriminator
+import eu.glasskube.operator.generic.dependent.GeneratedSecret
 import eu.glasskube.operator.odoo.Odoo
 import eu.glasskube.operator.odoo.OdooReconciler
 import eu.glasskube.operator.odoo.dbBackupSecretName
 import eu.glasskube.operator.odoo.dbBackupUsername
 import eu.glasskube.operator.odoo.resourceLabels
-import eu.glasskube.operator.secrets.SecretGenerator
-import io.fabric8.kubernetes.api.model.Secret
-import io.javaoperatorsdk.operator.api.reconciler.Context
-import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernetesDependentResource
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependent
-import io.javaoperatorsdk.operator.processing.dependent.kubernetes.ResourceUpdatePreProcessor
 
 @KubernetesDependent(
-    labelSelector = OdooReconciler.SELECTOR,
-    resourceDiscriminator = OdooDatabaseBackupSecret.Discriminator::class
+    labelSelector = OdooReconciler.SELECTOR
 )
-class OdooDatabaseBackupSecret :
-    CRUDKubernetesDependentResource<Secret, Odoo>(Secret::class.java),
-    ResourceUpdatePreProcessor<Secret> {
-
-    class Discriminator : NamedResourceDiscriminator<Secret, Odoo>({ dbBackupSecretName })
-
-    override fun desired(primary: Odoo, context: Context<Odoo>) = secret {
-        metadata {
-            name = primary.dbBackupSecretName
-            namespace = primary.metadata.namespace
-            labels = primary.resourceLabels + SecretGenerator.LABEL
-            annotations = mapOf(SecretGenerator.generateKeys("password"))
-        }
-        type = "kubernetes.io/basic-auth"
-        stringData = mapOf(
-            "username" to primary.dbBackupUsername
-        )
-    }
-
-    override fun replaceSpecOnActual(actual: Secret, desired: Secret, context: Context<*>) = actual.apply {
-        metadata.annotations.putAll(desired.metadata.annotations)
-        metadata.labels.putAll(desired.metadata.labels)
-        data.putAll(desired.data)
-        stringData.putAll(desired.stringData)
-    }
+class OdooDatabaseBackupSecret : GeneratedSecret<Odoo>() {
+    override val Odoo.generatedSecretName get() = dbBackupSecretName
+    override val Odoo.generatedSecretLabels get() = resourceLabels
+    override val generatedKeys get() = arrayOf("password")
+    override val Odoo.generatedSecretData get() = mapOf("username" to dbBackupUsername)
+    override val generatedSecretType = "kubernetes.io/basic-auth"
 }
