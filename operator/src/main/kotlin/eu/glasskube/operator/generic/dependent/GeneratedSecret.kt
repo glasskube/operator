5f364fc8095f99b9a575a417adc69ff6eeeceab4
Jakob Steiner <jakob.steiner@glasskube.eu>
2023-06-20 17:12:58 +0200

    fix: prevent excessive updates on secrets with generator annotation

--- operator/src/main/kotlin/eu/glasskube/operator/generic/dependent/GeneratedSecret.kt
+++ operator/src/main/kotlin/eu/glasskube/operator/generic/dependent/GeneratedSecret.kt
@@ -2,39 +2,37 @@ package eu.glasskube.operator.generic.dependent
 
 import eu.glasskube.kubernetes.api.model.metadata
 import eu.glasskube.kubernetes.api.model.secret
+import eu.glasskube.operator.encodeBase64
 import eu.glasskube.operator.infra.secrets.SecretGenerator
 import io.fabric8.kubernetes.api.model.HasMetadata
 import io.fabric8.kubernetes.api.model.Secret
 import io.javaoperatorsdk.operator.api.reconciler.Context
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernetesDependentResource
-import io.javaoperatorsdk.operator.processing.dependent.kubernetes.ResourceUpdatePreProcessor
+import kotlin.jvm.optionals.getOrNull
 
-abstract class GeneratedSecret<T : HasMetadata> :
-    CRUDKubernetesDependentResource<Secret, T>(Secret::class.java),
-    ResourceUpdatePreProcessor<Secret> {
-
+abstract class GeneratedSecret<T : HasMetadata> : CRUDKubernetesDependentResource<Secret, T>(Secret::class.java) {
     abstract val T.generatedSecretName: String
     abstract val T.generatedSecretLabels: Map<String, String>
     abstract val generatedKeys: Array<String>
     open val T.generatedSecretNamespace: String get() = metadata.namespace
     open val T.generatedSecretData: Map<String, String>? get() = null
-    open val generatedSecretType: String? get() = null
+    open val generatedSecretType = "Opaque"
 
     override fun desired(primary: T, context: Context<T>) = secret {
+        val existing = getSecondaryResource(primary, context)
+        val existingAnnotations = existing
+            .map { it.metadata.annotations.filterKeys { key -> key == SecretGenerator.GENERATED_KEYS } }
+            .getOrNull()
+            .orEmpty()
+        val existingData = existing.map { it.data }.getOrNull().orEmpty()
+
         metadata {
             name = primary.generatedSecretName
             namespace = primary.generatedSecretNamespace
             labels = primary.generatedSecretLabels + SecretGenerator.LABEL
-            annotations = mapOf(SecretGenerator.generateKeys(*generatedKeys))
+            annotations = existingAnnotations + mapOf(SecretGenerator.generateKeys(*generatedKeys))
         }
-        generatedSecretType?.let { type = it }
-        primary.generatedSecretData?.let { stringData = it }
-    }
-
-    override fun replaceSpecOnActual(actual: Secret, desired: Secret, context: Context<*>) = actual.apply {
-        metadata.annotations.putAll(desired.metadata.annotations)
-        metadata.labels.putAll(desired.metadata.labels)
-        data.putAll(desired.data)
-        stringData.putAll(desired.stringData)
+        type = generatedSecretType
+        data = existingData + primary.generatedSecretData?.mapValues { (_, it) -> it.encodeBase64() }.orEmpty()
     }
 }
