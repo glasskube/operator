62fc3c2863dc28cb1cc10077fe3d501d822b30b7
Jakob Steiner <jakob.steiner@glasskube.eu>
2023-06-20 13:54:06 +0200

    feat(Matomo): improve configuration and initial setup (#157)

--- operator/src/main/kotlin/eu/glasskube/operator/apps/matomo/dependent/MatomoConfigMap.kt
+++ operator/src/main/kotlin/eu/glasskube/operator/apps/matomo/dependent/MatomoConfigMap.kt
@@ -2,6 +2,7 @@ package eu.glasskube.operator.apps.matomo.dependent
 
 import eu.glasskube.kubernetes.api.model.configMap
 import eu.glasskube.kubernetes.api.model.metadata
+import eu.glasskube.operator.api.reconciler.getSecondaryResource
 import eu.glasskube.operator.apps.matomo.Matomo
 import eu.glasskube.operator.apps.matomo.MatomoReconciler
 import eu.glasskube.operator.apps.matomo.configMapName
@@ -9,8 +10,11 @@ import eu.glasskube.operator.apps.matomo.databaseName
 import eu.glasskube.operator.apps.matomo.databaseUser
 import eu.glasskube.operator.apps.matomo.mariaDBHost
 import eu.glasskube.operator.apps.matomo.resourceLabels
+import eu.glasskube.operator.logger
 import eu.glasskube.operator.resourceAsString
 import io.fabric8.kubernetes.api.model.ConfigMap
+import io.fabric8.kubernetes.api.model.apps.Deployment
+import io.fabric8.kubernetes.client.dsl.internal.apps.v1.RollingUpdater
 import io.javaoperatorsdk.operator.api.reconciler.Context
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernetesDependentResource
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependent
@@ -24,28 +28,42 @@ class MatomoConfigMap : CRUDKubernetesDependentResource<ConfigMap, Matomo>(Confi
             labels = primary.resourceLabels
         }
         data = mapOf(
-            "MATOMO_DATABASE_HOST" to host(primary),
+            "MATOMO_DATABASE_HOST" to primary.databaseHost,
             "MATOMO_DATABASE_USERNAME" to primary.databaseUser,
             "MATOMO_DATABASE_DBNAME" to primary.databaseName,
-            "init.sh" to initScript,
-            "install.json" to primary.installJson,
-            "glasskube-matomo-archive-cron" to primary.cron
+            "MATOMO_DATABASE_TABLES_PREFIX" to "matomo_",
+            "MATOMO_FIRST_USER_NAME" to "root",
+            "MATOMO_FIRST_USER_PASSWORD" to "glasskube-operator",
+            "MATOMO_FIRST_SITE_NAME" to "Example Site",
+            "MATOMO_FIRST_SITE_URL" to "www.example.com",
+            "MATOMO_INSTALL_FILE" to MatomoDeployment.installJsonPath,
+            MatomoDeployment.initSh to initSh,
+            MatomoDeployment.installSh to installSh,
+            MatomoDeployment.installJson to installJson,
+            MatomoDeployment.archiveCron to primary.archiveCron
         )
     }
 
-    private val initScript
-        get() = resourceAsString("init.sh")
-    private val Matomo.installJson
-        get() = replaceDatabaseValues(this@MatomoConfigMap.resourceAsString("config.json"), this)
-    private val Matomo.cron
+    override fun onUpdated(primary: Matomo, updated: ConfigMap, actual: ConfigMap, context: Context<Matomo>) {
+        super.onUpdated(primary, updated, actual, context)
+        context.getSecondaryResource<Deployment>().ifPresent {
+            log.info("restarting deployment after configmap changed")
+            RollingUpdater.restart(kubernetesClient.resource(it))
+        }
+    }
+
+    private val initSh get() = resourceAsString("init.sh")
+    private val installSh get() = resourceAsString("install.sh")
+    private val installJson get() = this.resourceAsString("config.json")
+    private val Matomo.archiveCron
         get() = replaceHost(this@MatomoConfigMap.resourceAsString("cron"), this)
 
     private fun replaceHost(cron: String, primary: Matomo) = cron.replace("%HOST%", primary.spec.host!!)
 
-    private fun replaceDatabaseValues(config: String, primary: Matomo) =
-        config.replace("%MATOMO_DATABASE_HOST%", host(primary))
-            .replace("%MATOMO_DATABASE_USERNAME%", primary.databaseUser)
-            .replace("%MATOMO_DATABASE_DBNAME%", primary.databaseName)
+    private val Matomo.databaseHost get() = "$mariaDBHost.${metadata.namespace}"
 
-    private fun host(primary: Matomo) = "${primary.mariaDBHost}.${primary.metadata.namespace}"
+    companion object {
+        @JvmStatic
+        private val log = logger()
+    }
 }
--- operator/src/main/resources/eu/glasskube/operator/apps/matomo/dependent/init.sh
+++ operator/src/main/resources/eu/glasskube/operator/apps/matomo/dependent/init.sh
@@ -1,28 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-abort() {
-  echo "$1"
-  exit 1
-}
+echo "
+[General]
+always_load_commands_from_plugin=ExtraTools
+" >/usr/src/matomo/config/config.ini.php
 
-ls -la /tmp/matomo
-mkdir /tmp/install
-cp /tmp/matomo/install.json /tmp/install/install.json
-sed -i "s/%MATOMO_DATABASE_PASSWORD%/$MATOMO_DATABASE_PASSWORD/g" /tmp/install/install.json
-rsync -crlOt --no-owner --no-group --no-perms /usr/src/matomo/ /var/www/html/
-
-./console plugin:activate ExtraTools ||
-  abort "could not activate ExtraTools"
-./console matomo:install --install-file=/tmp/install/install.json --force --do-not-drop-db ||
-  abort "could not install database"
-
-./console site:list
-
-if [ $? -eq 1 ]; then
-  echo "Init first test page"
-  ./console site:add --name="MTL Test Site" --urls="https://glasskube.media-lab.de" ||
-    abort "could not add site"
-fi
-
-chown -R www-data:www-data /var/www/html
-echo "done"
+rsync --checksum --recursive --links --times --omit-dir-times --no-owner --no-group --no-perms \
+  /usr/src/matomo/ /var/www/html/
