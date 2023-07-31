abc07f5c1d75b63da41c536a02551bc67e385b50
Jakob Steiner <jakob.steiner@glasskube.eu>
2023-07-31 16:20:17 +0200

    fix(Vault): exit if auth request fails (#162)

--- operator/src/main/resources/eu/glasskube/operator/apps/vault/dependent/run-with-auth.sh
+++ operator/src/main/resources/eu/glasskube/operator/apps/vault/dependent/run-with-auth.sh
@@ -1,4 +1,13 @@
 SERVICE_ACCOUNT_TOKEN="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
+
 VAULT_TOKEN=$(
     vault write auth/kubernetes/login "role=$AUTH_ROLE_NAME" "jwt=$SERVICE_ACCOUNT_TOKEN" | sed -nr "s/token\s+(\S*)/\1/p"
-) /usr/local/bin/docker-entrypoint.sh vault server -config=/glasskube/config/config.hcl
+)
+
+if [ -z "$VAULT_TOKEN" ]; then
+  echo "authentication failed. exiting."
+  exit 1
+fi
+
+export VAULT_TOKEN
+/usr/local/bin/docker-entrypoint.sh vault server -config=/glasskube/config/config.hcl
