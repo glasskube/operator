cef6d2071d00a69fb38a1babb2b3a841b51b6dcc
christoph <christoph.enne@glasskube.eu>
2023-12-07 15:20:01 +0100

    feat(Gitlab): provide more options for registry storage configuration (#447)

--- operator/src/main/kotlin/eu/glasskube/operator/apps/gitlab/GitlabRegistryStorageSpec.kt
+++ operator/src/main/kotlin/eu/glasskube/operator/apps/gitlab/GitlabRegistryStorageSpec.kt
@@ -1,6 +1,5 @@
 package eu.glasskube.operator.apps.gitlab
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import eu.glasskube.operator.apps.common.cloudstorage.CloudStorageSpec
 import io.fabric8.generator.annotation.Required
 import io.fabric8.kubernetes.api.model.SecretKeySelector
@@ -17,15 +16,9 @@ data class GitlabRegistryStorageSpec(
         override val secretKeySecret: SecretKeySelector,
         @field:Required
         override val region: String,
-        @field:Required
         override val hostname: String,
-        @field:Required
-        override val usePathStyle: Boolean
-    ) : CloudStorageSpec {
-        @field:JsonIgnore
-        override val port = null
-
-        @field:JsonIgnore
-        override val useSsl = true
-    }
+        override val port: Int?,
+        override val useSsl: Boolean,
+        override val usePathStyle: Boolean = false
+    ) : CloudStorageSpec
 }
