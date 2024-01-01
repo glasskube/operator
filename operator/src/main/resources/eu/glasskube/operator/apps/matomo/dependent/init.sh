4e373c2159e587a1ea036b9b1d7aec0283dd11b2
Philip Miglinci <pmig@glasskube.eu>
2024-01-01 21:37:41 +0100

    feat(Matomo): prepare deployment for Matomo 5 release
    Signed-off-by: Philip Miglinci <pmig@glasskube.eu>

--- operator/src/main/resources/eu/glasskube/operator/apps/matomo/dependent/init.sh
+++ operator/src/main/resources/eu/glasskube/operator/apps/matomo/dependent/init.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env sh
 
 echo "
+
 [General]
 always_load_commands_from_plugin=ExtraTools
-" >/usr/src/matomo/config/config.ini.php
+force_ssl = 1
+" >> /usr/src/matomo/config/config.ini.php
+
+rsync --checksum --recursive --links --times --omit-dir-times --no-owner --no-group --no-perms --delete \
+  /usr/src/matomo/ /var/www/html/ \
+  --exclude=tmp --exclude=misc
 
 rsync --checksum --recursive --links --times --omit-dir-times --no-owner --no-group --no-perms \
-  /usr/src/matomo/ /var/www/html/
+  /usr/src/matomo/misc/ /var/www/html/misc
--- operator/src/main/resources/eu/glasskube/operator/apps/matomo/dependent/install.sh
+++ operator/src/main/resources/eu/glasskube/operator/apps/matomo/dependent/install.sh
@@ -7,7 +7,7 @@ abort() {
 
 ./console matomo:install --install-file "$MATOMO_INSTALL_FILE" --force --do-not-drop-db || abort "could not install matomo"
 
-./console plugin:activate TagManager || abort "could not install tag manager"
+./console core:update --yes || abort "upgrade failed"
 
 if ./console site:list; then
   echo "site:list exit code was 0. do not create a new site"
@@ -16,5 +16,12 @@ else
   ./console site:add --name "$MATOMO_FIRST_SITE_NAME" --urls "$MATOMO_FIRST_SITE_URL" || abort "could not add site"
 fi
 
-./console core:convert-to-utf8mb4 --yes || abort "could not convert database"
+# Note: if the command returns Command "core:convert-to-utf8mb4" is not defined.
+# then your database should already be using utf8mb4 and you don’t need to run the command.
+# https://matomo.org/faq/how-to-update/how-to-convert-the-database-to-utf8mb4-charset/
+
+./console core:convert-to-utf8mb4 --yes || echo "could not convert database or database already converted"
 ./console core:create-security-files || abort "could not create security files"
+
+./console plugin:activate TagManager || abort "could not install tag manager"
+./console plugin:deactivate ProfessionalServices Marketplace Feedback || echo "could not uninstall professional services, marketplace and feedback"
