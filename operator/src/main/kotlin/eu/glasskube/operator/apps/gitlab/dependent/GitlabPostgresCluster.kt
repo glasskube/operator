85b01bd76014510c2701672cb236fa79ea8e077c
Jakob Steiner <kosmoz@users.noreply.github.com>
2026-08-17 12:02:28 +0200

    fix(gitlab): bump database memory to 450Mi

--- operator/src/main/kotlin/eu/glasskube/operator/apps/gitlab/dependent/GitlabPostgresCluster.kt
+++ operator/src/main/kotlin/eu/glasskube/operator/apps/gitlab/dependent/GitlabPostgresCluster.kt
@@ -15,5 +15,5 @@ class GitlabPostgresCluster(configService: ConfigService) :
 
     override val Gitlab.databaseOwnerName get() = "gitlab"
     override val Gitlab.defaultStorageSize get() = "20Gi"
-    override val Gitlab.databaseMemory get() = Quantity("384", "Mi")
+    override val Gitlab.databaseMemory get() = Quantity("450", "Mi")
 }
